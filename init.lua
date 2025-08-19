dba282b3fbf92faa22ff2a3968dc8b4da90a9ccf
dan <dan@nomail.com>
2025-08-19 19:57:47 +1200

    change keybinding for mini.surround to <c-s>

--- init.lua
+++ init.lua
@@ -23,7 +23,6 @@
 --]]
 
 -- My custom settings
-
 vim.opt.spell = true
 vim.opt.spelllang = 'en_nz'
 vim.opt.colorcolumn = '80'
@@ -879,7 +878,20 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = '<C-s>a', -- Add surrounding in Normal and Visual modes
+          delete = '<C-s>d', -- Delete surrounding
+          find = '<C-s>f', -- Find surrounding (to the right)
+          find_left = '<C-s>F', -- Find surrounding (to the left)
+          highlight = '<C-s>h', -- Highlight surrounding
+          replace = '<C-s>r', -- Replace surrounding
+          update_n_lines = '<C-s>n', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
