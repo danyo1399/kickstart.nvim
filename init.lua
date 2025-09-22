6634e5bfb9650685c448666c8c91d0ec10d3e59c
dan <dan@nomail.com>
2025-09-22 12:12:31 +1200

    add sh formatting

--- init.lua
+++ init.lua
@@ -630,6 +630,7 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
+        sh = { 'shfmt' },
         dockerfile = { 'dockerfmt' },
         markdown = { 'markdownfmt' },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
