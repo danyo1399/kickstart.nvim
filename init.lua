4b5ac7fb9b0b5a138c34ef55196556efdfa10463
dan <dan@nomail.com>
2025-09-23 23:47:38 +1200

    add yaml formatter

--- init.lua
+++ init.lua
@@ -631,6 +631,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         sh = { 'shfmt' },
+        yaml = {'yamlfmt'},
         dockerfile = { 'dockerfmt' },
         markdown = { 'markdownfmt' },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
